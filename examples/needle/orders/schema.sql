cb5921f7d3efadd00e1967edc7b02042e70815ca
Yumin Xia <me@yumin-xia.com>
2022-12-21 15:30:50 -0800

    add needle example

---
+++ examples/needle/orders/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS Orders (
+   ID        INT GENERATED ALWAYS AS IDENTITY,
+   UserID    INT references Users(ID) NOT NULL,
+   ItemID    INT references Items(ID) NOT NULL,
+   CreatedAt TIMESTAMP NOT NULL DEFAULT NOW(),
+   IsDeleted BOOLEAN NOT NULL,
+   PRIMARY KEY(ID)
+);
