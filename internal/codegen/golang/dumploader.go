25cf3498ff0905a96b1fd842cdafeda7beaacda6
Yumin Xia <me@yumin-xia.com>
2023-01-29 16:11:25 -0800

    fix: dumper will not try to sort by non-orderable columns like JSON

--- internal/codegen/golang/dumploader.go
+++ internal/codegen/golang/dumploader.go
@@ -35,20 +35,41 @@ func (d DumpLoader) FieldDBNames() string {
 	return strings.Join(fields, ",")
 }
 
+func (d DumpLoader) DumpSortByFields() string {
+	if d.MainStruct == nil {
+		panic("no MainStruct in DumpLoader")
+	}
+	var fields []string
+	for _, f := range d.MainStruct.Fields {
+		switch f.Type {
+		// TODO(yumin):
+		// best-effort sorting for now. Once we pass table indexes to codegen
+		// we can just use index information.
+		case "int", "int16", "int32", "int64", "float32", "float64", "string", "bool", "time.Time":
+			fields = append(fields, f.DBName)
+		case "*int", "*int16", "*int32", "*int64", "*float32", "*float64", "*string", "*bool", "*time.Time":
+			fields = append(fields, f.DBName)
+		default:
+			continue
+		}
+	}
+	return strings.Join(fields, ",")
+}
+
 func (d DumpLoader) ParamList() string {
 	if d.MainStruct == nil {
 		panic("no MainStruct in DumpLoader")
 	}
 	var vals []string
 	for i := range d.MainStruct.Fields {
-		vals = append(vals, fmt.Sprintf("$%d", i + 1))
+		vals = append(vals, fmt.Sprintf("$%d", i+1))
 	}
 	return strings.Join(vals, ",")
 }
 
 func (d DumpLoader) DumpSQL() string {
 	return fmt.Sprintf("SELECT %s FROM %s ORDER BY %s ASC;",
-		d.FieldDBNames(), d.MainStruct.Table.Name, d.FieldDBNames())
+		d.FieldDBNames(), d.MainStruct.Table.Name, d.DumpSortByFields())
 }
 
 func (d DumpLoader) LoadSQL() string {
